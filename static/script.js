f60a9f246d392b4a5a19dff198d1641477bfd84b
Adrian <adpenagosa@gmail.com>
2026-03-17 20:41:00 -0600

    edit script.js agregué la forma para recibir los mensajes y que se lean en formato de lista

---
+++ static/script.js
@@ -0,0 +1,20 @@
+const getMessages = async () => {
+    const chatBox = document.getElementById("chat-box");
+
+    const response = await fetch("/api/messages");
+    const messages = await response.json();
+
+    chatBox.innerHTML = "";
+
+    for (let i = 0; i < messages.length; i++) {
+        const message = messages[i];
+
+        const li = document.createElement("li");
+        li.innerHTML = `<strong>${message.author || message.user}:</strong> ${message.text}`;
+
+        chatBox.appendChild(li);
+    }
+
+
+};
+
