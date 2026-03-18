79ca7255b6ba44fd097900fa2af070795b9c4646
Adrian <adpenagosa@gmail.com>
2026-03-17 20:35:43 -0600

    edit main.go: agregué los métodos para postear y recibir mensajes

--- main.go
+++ main.go
@@ -27,6 +27,17 @@ func postMessages(){
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("static")))
 
+	http.HandleFunc("/api/messages", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodGet {
+			getMessages(w, r)
+			return
+		}
+		if r.Method == http.MethodPost {
+			postMessages(w, r)
+			return
+		}
+	})
+
 	fmt.Println("Servidor iniciado en el puerto 8000...")	
 	http.ListenAndServe("0.0.0.0:8000", nil)
 }
